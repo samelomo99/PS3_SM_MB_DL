ce2db6d9888a1293829a91b37d619460a3c3e8ea
Miguel Blanco <m.blancog@uniandes.edu.co>
2025-05-19 00:48:53 -0500

    cambios

---
+++ script/PS3_script_main.R
@@ -0,0 +1,50 @@
+#-------------------------------------------------------------------#
+## --------------- Problem Set 3: Making Money with ML ------------- ##
+## - Santiago Melo - Miguel Blanco - Diana Lopera -------------------##
+#-------------------------------------------------------------------#
+
+
+# ---------- LIBRERÍAS ---------- # ----
+
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+
+pacman::p_load(
+  readr,        # Importar datos (ya incluido en tidyverse)
+  labelled,     # Manejo de etiquetas
+  naniar,       # Visualizar datos faltantes
+  DataExplorer, # Gráficos de missing values
+  psych,        # Estadísticas descriptivas
+  rvest,        # Web scraping
+  rio,          # Importar/exportar datos
+  tidyverse,    # Conjunto de paquetes para tidy data (incluye dplyr, ggplot2, etc.)
+  skimr,        # Resumen de datos
+  visdat,       # Visualizar datos faltantes
+  corrplot,     # Gráficos de correlación
+  gridExtra,    # Organización de gráficos
+  MASS,         # Funciones estadísticas diversas
+  stargazer,    # Tablas para salida a TEX
+  chromote,     # Automatización de navegador (útil para scraping avanzado)
+  ggplot2,      # Gráficos (ya incluido en tidyverse)
+  boot,         # Funciones de bootstrap
+  patchwork,    # Combinación de gráficos
+  caret,         # For predictive model assessment
+  purrr,
+  kableExtra,   # Opciones adicionales para kable()
+  dplyr,          # Manipulación de datos
+  summarytools,
+  knitr,          # kable() para generar tablas en LaTeX
+  xtable,
+  tidyr,
+  gmodels,
+  glmnet,
+  ranger, # Para bagging y random forest
+  randomForest, # Para random forest
+  tidyverse, # tidy-data
+  caret ,  # for model training and tunning
+  Metrics, ## Evaluation Metrics for ML
+  adabag
+)
+
+
+# ---------- BASE DE DATOS ---------- # ----
